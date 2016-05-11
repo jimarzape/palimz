d9f6f4114f9db0160042713779dbf904cd93b784
Jimar <Jimar>
2016-05-11 15:53:35 +0800

    test

---
+++ js/login.js
@@ -0,0 +1,48 @@
+
+
+$('#btnLogin').click(function(){
+	var username = $('#txtUsername').val();
+	var password = $('#txtPassword').val();
+	if(username != "" && password != ""){
+		$('.fillmissing').css("display","none");
+		$.ajax({
+			type       : "POST",
+            url        : "http://192.168.0.115:8080/palimex/log.php",
+            crossDomain: true,
+            beforeSend : function() {$('.loadImg').css("display","block");},
+            complete   : function() {$('.loadImg').css("display","none");},
+            data       : { 'user':username, 'pass':password},
+            // dataType   : 'json',
+            success    : function(response) {
+ 
+                var json = $.parseJSON(response);
+                var j = [];
+                var ss = 0;
+                $(json).each(function(i,val){
+                    $.each(val, function(k,v){
+                        j[ss] = v;
+                        ss++;
+                        console.log(v);
+
+                    });
+
+                });       
+            },
+            error      : function() {
+                //console.error("error");
+                alert('Not working!');                  
+            }
+		});
+	}
+	else{
+		var fill= "Please fill the missing field/s.";
+		$('.fillmissing').html(fill);
+		$('.fillmissing').css("display","block");
+	}
+});
+$('#btnExit').click(function(){
+	navigator.app.exitApp();
+});
+$('.divClose').click(function(){
+	$('.pop-up').css("display","none");
+});
